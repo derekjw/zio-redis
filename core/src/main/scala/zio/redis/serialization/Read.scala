f0c8f7f066c35cb56a4d78ead093640f969a14c2
Derek Williams <derek@nebvin.ca>
2019-11-07 23:11:19 +0000

    add string serializer

--- core/src/main/scala/zio/redis/serialization/Read.scala
+++ core/src/main/scala/zio/redis/serialization/Read.scala
@@ -1,6 +1,10 @@
 package zio.redis.serialization
 
+import java.nio.charset.Charset
+
 import zio.Chunk
+
+import scala.util.control.NonFatal
 
 case class SerializationFailure(message: String, cause: Option[Throwable])
     extends RuntimeException(message, cause.orNull)
@@ -11,4 +15,19 @@ trait Read[A] {
 
 object Read {
   type Result[A] = Either[SerializationFailure, A]
+
+  @inline
+  def apply[A](chunk: Chunk[Byte])(implicit read: Read[A]): Result[A] = read(chunk)
+
+  implicit val readString: Read[String] = {
+    val charset = Charset.forName("UTF8")
+
+    { chunk =>
+      try {
+        Right(new String(chunk.toArray, charset))
+      } catch {
+        case NonFatal(e) => Left(SerializationFailure("Failed to parse as string", Some(e)))
+      }
+    }
+  }
 }
--- core/src/main/scala/zio/redis/serialization/Write.scala
+++ core/src/main/scala/zio/redis/serialization/Write.scala
@@ -1,7 +1,22 @@
 package zio.redis.serialization
 
+import java.nio.charset.Charset
+
 import zio.Chunk
 
 trait Write[A] {
   def apply(in: A): Chunk[Byte]
+}
+
+object Write {
+  @inline
+  def apply[A](in: A)(implicit write: Write[A]): Chunk[Byte] = write(in)
+
+  implicit val writeString: Write[String] = {
+    val charset = Charset.forName("UTF8")
+
+    { str =>
+      Chunk.fromArray(str.getBytes(charset))
+    }
+  }
 }
